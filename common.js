9222eb5a7380c700b92e37a080123d7efe439bd4
clinomics-IamKira <ykkee7@clinomics.co.kr>
2020-05-15 11:04:08 +0900

    인트로 추가

--- common.js
+++ common.js
@@ -1,23 +1,36 @@
 const initScrollMagic = () => {
   const controller = new ScrollMagic.Controller();
 
+  //헤더 왼쪽 문구
   const headerLeftText1 = TweenMax.to('.leftText1', 0.5, {
     x: '-100%',
     alpha: 0,
   });
 
+  //헤더 왼쪽 로고
   const headerLeftText2 = TweenMax.to('.leftText2', 0.5, {
     x: '100%',
     alpha: 1,
   });
 
+  //헤더 오른쪽 문구
   const headerRightText = TweenMax.to('.rightText', 0.5, {
     scale: 0.8,
     rotation: 90,
     x: 50,
-    y: 70,
+    y: 30,
     alpha: 0.7,
     color: '#555a61',
+  });
+
+  const introText1 = TweenMax.to('.introText1', 0.5, {
+    y: -30,
+    alpha: 1,
+  });
+
+  const introText2 = TweenMax.to('.introText2', 0.5, {
+    y: -30,
+    alpha: 1,
   });
 
   const lightOff = TweenMax.to('.light-off', 0.5, {
@@ -58,21 +71,24 @@ const initScrollMagic = () => {
     duration: '30%',
   })
     .setTween([headerLeftText1, headerLeftText2, headerRightText])
-    .addTo(controller)
-    .addIndicators({
-      name: 'header',
-    });
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerHook: 0.1,
+    offset: "200%",
+    duration: '90%',
+  })
+    .setTween([introText2])
+    .addTo(controller);
 
   new ScrollMagic.Scene({
     triggerHook: 0,
     duration: '90%',
   })
-    .setPin('.mainLogo')
-    .setTween([lightOff, lightOn, mainLogoText1, mainLogoText2, mainLogo])
-    .addTo(controller)
-    .addIndicators({
-      name: 'mainLogo',
-    });
+    // .setPin('.mainLogo')
+      .setPin('.section-1')
+    .setTween([lightOff, lightOn, introText1, mainLogoText1, mainLogoText2, mainLogo])
+    .addTo(controller);
 };
 
 $(document).ready(() => {
