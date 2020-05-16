1f42aaa78aa0e9442aa1c9081b32909d274bd18f
KeraKim <kerakim0716@gmail.com>
2020-05-16 20:46:06 +0900

    포트폴리오 ver.1 완성

--- common.js
+++ common.js
@@ -70,8 +70,7 @@ const initScrollMagic = () => {
       scale: 0.85,
     },
     {
-      // x: browserType == 'pc' ? ()
-      x: browserType == 'pc' ? -180 : (browserType == 'tablet' ? -150 : '-27vw'),
+      x: browserType == 'pc' ? -180 : browserType == 'tablet' ? -150 : '-20vw',
       backgroundColor: 'rgba(255, 212, 212)',
       scale: 1.2,
       rotation: 360,
@@ -81,12 +80,54 @@ const initScrollMagic = () => {
   );
 
   const mainLogo = TweenMax.to('.mainLogo', 0.5, {
-    x: browserType == 'pc' ? -200 : (browserType == 'tablet' ? -50 : -20),
+    x: browserType == 'pc' ? -200 : browserType == 'tablet' ? -50 : -20,
   });
 
   const section1 = TweenMax.to('.section-1', 0.5, {
     alpha: 0,
   });
+
+  const titleText = TweenMax.staggerFromTo(
+    '.titleText1, .titleText2, .titleText3, .titleText4',
+    0.4,
+    {
+      alpha: 0.5,
+    },
+    {
+      alpha: 1,
+    },
+    0.3
+  );
+
+  const skill = TweenMax.staggerFromTo(
+    '.skill',
+    0.7,
+    {
+      y: '45px',
+    },
+    {
+      y: 0,
+    },
+    0.2
+  );
+
+  const profileImage = TweenMax.to('.profileImageWrap', 0.5, {
+    y: -100,
+  });
+
+  const closeText = TweenMax.staggerFromTo(
+    '.closeText',
+    0.7,
+    {
+      x: '-50px',
+      alpha: 0.5,
+    },
+    {
+      x: 0,
+      alpha: 1,
+    },
+    0.2
+  );
 
   new ScrollMagic.Scene({
     triggerHook: 0,
@@ -116,11 +157,47 @@ const initScrollMagic = () => {
     duration: '30%',
   })
     .setTween(section1)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: '.section-2',
+    offset: '-100%',
+    duration: '30%',
+  })
+    .setTween(titleText)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: '.section-2',
+    offset: '-200%',
+    duration: '50%',
+  })
+    .setTween(profileImage)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: '.section-3',
+    duration: '80%',
+    offset: '180%',
+  })
+    .setTween(skill)
+    .addTo(controller);
+
+  new ScrollMagic.Scene({
+    triggerElement: '.section-4',
+    duration: '30%',
+    offset:
+      browserType == 'pc' ? '-80%' : browserType == 'tablet' ? '-80%' : '40%',
+  })
+    .setTween(closeText)
     .addTo(controller)
+    // .addIndicators({
+    //   name: 'header',
+    // });
 };
 
 $(document).ready(() => {
-  // $('.year').text(new Date().getFullYear() - 2016);
+  $('.year').text(new Date().getFullYear() - 2016);
 
   //브라우저 타입 구분
   getBrowserType();
@@ -133,127 +210,3 @@ $(document).ready(() => {
   //매직스크롤 초기화
   initScrollMagic();
 });
-
-const initScrollMagic1 = () => {
-  const controller = new ScrollMagic.Controller();
-
-  const titleText = TweenMax.to(
-    '.titleText1, .titleText2, .titleText3, .titleText4',
-    0.5,
-    {
-      alpha: 1,
-    }
-  );
-
-  const profileImage = TweenMax.to('.profileImageWrap', 0.5, {
-    y: -100,
-  });
-
-  const skill = TweenMax.staggerFromTo(
-    '.skill',
-    0.7,
-    {
-      y: '45px',
-    },
-    {
-      y: 0,
-    },
-    0.2
-  );
-
-  const closeText = TweenMax.staggerFromTo(
-    '.closeText',
-    0.7,
-    {
-      x: '-50px',
-      alpha: 0.5,
-    },
-    {
-      x: 0,
-      alpha: 1,
-    },
-    0.2
-  );
-
-  new ScrollMagic.Scene({
-    triggerElement: '.section-3',
-    duration: '50%',
-    offset: '40%',
-  })
-    .setTween(skill)
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerHook: 0,
-    duration: '30%',
-  })
-    .setTween([headerLeftText1, headerLeftText2, headerRightText])
-    .addTo(controller);
-  // .addIndicators({
-  //   name: 'header',
-  // });
-
-  new ScrollMagic.Scene({
-    triggerHook: 0.2,
-    offset: '200%',
-    duration: '90%',
-  })
-    .setTween([introText2])
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerHook: 0,
-    duration: '90%',
-  })
-    .setPin('.section-1')
-    .setTween([
-      lightOff,
-      lightOn,
-      introText1,
-      mainLogoText1,
-      mainLogoText2,
-      mainLogo,
-    ])
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    // triggerHook: 0.2,
-    triggerElement: '.section-2',
-    offset: '-200%',
-    duration: '30%',
-  })
-    .setTween(titleText)
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    // triggerHook: 0.2,
-    triggerElement: '.section-2',
-    offset: '-170%',
-    duration: '50%',
-  })
-    .setTween(profileImage)
-    .addTo(controller);
-
-  new ScrollMagic.Scene({
-    triggerElement: '.section-4',
-    duration: '30%',
-    offset: '-80%',
-  })
-    .setTween(closeText)
-    .addTo(controller);
-};
-
-// $(document).ready(() => {
-//
-//   getBrowserType();
-//   console.log(browserType)
-//
-//   $('.year').text(new Date().getFullYear() - 2016);
-//
-//
-//   $('.leftText2').click(() => {
-//     $('html, body').animate({ scrollTop: 0 }, 400);
-//   });
-//
-//   initScrollMagic();
-// });
